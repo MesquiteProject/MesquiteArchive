53f4a1429243c6c637e06dc5cccd84c2e2652b02
DMaddison <david.maddison@science.oregonstate.edu>
2008-06-09 05:29:45 +0000

    removing 2.01

--- releases/current/Mesquite Project/Resources/Run_Mesquite_OSX.command
+++
@@ -1,2 +0,0 @@
-dir=`dirname "$0"`
-java  -Xmx400M -Djava.library.path=lib -cp "$dir" mesquite.Mesquite
--- releases/current/Mesquite Project/Source/mesquite/lib/OutputFileProcessor.java
+++
@@ -1,9 +0,0 @@
-package mesquite.lib;
-
-public interface OutputFileProcessor {
-	
-	public void processOutputFile(String[] outputFilePaths, int fileNum);
-	
-	public void processCompletedOutputFiles(String[] outputFilePaths);
-
-}
